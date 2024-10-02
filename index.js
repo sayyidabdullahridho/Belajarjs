68e88d57bae555a54be7e93b11d35d6364ea3e8f
SAYYID ABDULLAH RIDHO SAPUTRA <110073538+sayyidabdullahridho@users.noreply.github.com>
2024-10-02 12:41:40 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+const readline = require("readline-sync");
+
+const angkaPertama = parseFloat(readline.question("Masukkan Angka Pertama :" ));
+const operator = readline.question("Masukkan Operator (+,-,*,/) :" );
+const angkaKedua = parseFloat(readline.question("Masukkan Angka Kedua:" ));
+
+if (isNaN(angkaPertama) || isNaN(angkaKedua)){
+    return console.log("Inputan tidak sesuai !!!");
+} else {
+
+
+const hasil = execute(angkaPertama,angkaKedua,operator);
+console.log(`Hasilnya adalah ${hasil}`);
+
+}
+
+function execute(angkaPertama,angkaKedua,operator){
+    switch (operator) {
+        case "+":
+            return angkaPertama + angkaKedua;
+        case "-":
+            return angkaPertama - angkaKedua;
+        case "*":
+            return angkaPertama * angkaKedua;
+        case "/":
+            if (angkaKedua === 0){
+                return console.log("Error: Tidak bisa dibagi dengan nol")
+            }
+
+            return angkaPertama / angkaKedua;
+        case "%":
+            return angkaPertama % angkaKedua;
+        default:
+            console.log("operator tidak valid");
+
+
+    }
+}
